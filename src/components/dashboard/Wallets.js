25e6fa228292ea528593edf3ce91a6ab0aab0dc9
RAnastasiya <rizhko.anastasiya@gmail.com>
2018-04-30 10:27:24 +0300

    add Wallets block

--- src/components/dashboard/Wallets.js
+++ src/components/dashboard/Wallets.js
@@ -1,10 +1,40 @@
 import React, { Component } from "react";
+import FaBitcoin from "react-icons/lib/fa/bitcoin";
+import Currencies from "../../constants/Currencies";
 
 class Wallets extends Component {
     render() {
 
         return (
           <div className="block-wallets block">
+            <div className="wallets-add">
+              <p>My wallets</p>
+              <button>Add wallet</button>
+            </div>
+            <div className="wallets-values">
+              <div className="currency-of-wallet-values">
+                <div>
+                  <div>
+                    <p>BTC</p>
+                    <FaBitcoin />
+                  </div>
+                  <div>
+                    <p>Bitcoin</p>
+                    <p>0.221746</p>
+                  </div>
+                </div>
+                <Currencies />
+              </div>
+              <div className="currency-of-wallet">
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+              </div>
+            </div>
+
           </div>
         );
     }
